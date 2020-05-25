7d76098383f723f6496db5a4e21e0df6af848cc6
star123good <star921str@hotmail.com>
2020-05-26 03:49:43 +0800

    complete

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function() {
+    $('.carousel').carousel({
+        carouselWidth: 930,
+        carouselHeight: 200,
+        directionNav: true,
+        reflection: false,
+        shadow: false,
+        buttonNav: 'bullets',
+        hMargin: 0.9,
+        vMargin: 0.5,
+        frontWidth: 200,
+        frontHeight: 150,
+        hAlign:'center',
+        vAlign:'center',
+    });
+});
